b5dffa0b7aa91e9ea4eef3f48e06115fcbf5bf1f
ednilsoncampos <ednilsoncampos@gmail.com>
2026-03-31 22:25:07 -0300

    Ajusta proxy para corrigir falha de cors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: '/api/v1',
   devCredentials: {
     email: 'ednilsoncampos@gmail.com',
-    senha: 'A123456',
+    senha: 'dev@123',
   },
 };
